624e8fa69b645d96ebf506baa7e42ce3da8f3e32
Jesus Bejar <israelbejarii@gmail.com>
2024-01-13 08:56:33 -0700

    added router check and comments

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 app.get('/', (req, res) =>(
     res.send('Hola Mundo!')
 ));
+// checks for traffic in routes/index.js
+app.use('/', require('./routes'));
 
 const port = 3000;
+// listens for traffic on port 3000
 app.listen(process.env.port || port);
 console.log('Server is listening at port ' + (process.env.port || port));
