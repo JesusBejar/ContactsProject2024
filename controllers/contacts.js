409ab809076090666014ba4e672c67ac9c049a04
Jesus Bejar <israelbejarii@gmail.com>
2024-01-13 19:45:46 -0700

    export functions

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,9 +11,14 @@ const getAll = async = (req, res) => {
 
 const getSingle = async = (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-type', 'application/json');
         res.status(200).json(contact[0]);
     }).catch();
+};
+
+module.exports = {
+    getAll, 
+    getSingle
 };
