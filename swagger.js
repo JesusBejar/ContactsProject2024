1bd544c8ca529368cb18243088d7c6c4111873fe
Jesus Bejar <israelbejarii@gmail.com>
2024-01-18 15:43:26 -0700

    installed swagger autogen & ui

---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: "Contacts API",
+        description: "Made 2024 in the CSE341 class"
+    },
+    host: "locahost:3000",
+    schemes: ["https"]
+};
+const outputfile = './swagger.json';
+const endpointsfile = ['./routesindex.js'];
+
+swaggerAutogen(outputfile, endpointsfile, doc);
