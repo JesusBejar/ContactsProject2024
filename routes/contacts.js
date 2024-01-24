8e9308a01730e6ce311e72bbfd2e11b9bcaadeb7
Jesus Bejar <israelbejarii@gmail.com>
2024-01-24 13:22:53 -0700

    added some paths for validation

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 const contactsController = require('../controllers/contacts');
+const validate = require('../middleware/validate');
+
 // these match with functions in ./controllers/contacts.js
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
 // CREATE
-router.post('/', contactsController.createUser)
+router.post('/', validate.saveContact, contactsController.createUser)
 // UPDATE
-router.put('/', contactsController.updateUser)
+router.put('/:id', validate.saveContact, contactsController.updateUser)
 // DELETE
 router.delete('/', contactsController.deleteUser)
 
